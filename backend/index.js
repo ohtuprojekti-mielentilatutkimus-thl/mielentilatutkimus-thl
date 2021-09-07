eaa1661d7f4d10f7c933136d3b24a55a6e9155bf
ArtKoski <arcez@hotmail.fi>
2021-09-07 10:56:15 +0300

    add: eslint

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Terve</h1>')
+    res.send('<h1>Terve</h1>')
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`)
+    console.log(`Running on port ${PORT}`)
 })
