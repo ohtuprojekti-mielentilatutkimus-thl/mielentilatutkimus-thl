c6dcc2011f953a1e158ef73f04e547a5afdeae84
Jussi M <jussmaki@cs.helsinki.fi>
2021-09-14 17:25:24 +0300

    Start of backend

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,30 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/mielentila')
+
+app.use(express.json())
+//app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.send('<h1>Testi</h1>')
+    res.json({message: 'moi maailma'})
+    //res.send('<h1>Testi</h1>')
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
 })
+
+const Form = require('./models/form.model.js')
+app.post('/', async (req, res) => {
+    const data = req.body
+    const form = new Form({
+        hello: data.hello
+    })
+    const savedForm = await form.save()
+    res.json(savedForm.toJSON())
+})
+
+
