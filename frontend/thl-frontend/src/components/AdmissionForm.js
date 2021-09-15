01cad2aad49cee0faf197da49ed4d0c4bbc3b1ed
uradora <meri.rajamaki@gmail.com>
2021-09-15 16:56:17 +0300

    lomakkeen kentät lisätty

--- frontend/thl-frontend/src/components/AdmissionForm.js
+++ frontend/thl-frontend/src/components/AdmissionForm.js
@@ -2,7 +2,41 @@ import React from 'react'
 
 const AdmissionForm = ({ form }) => {
     <div>
-      heippa lomake
+      <p>lomakkeen tunnus: {form.id}</p>
+      {form.name}
+      {form.lastname}
+      {form.identificationNumber}
+      {form.address}
+      {form.location}
+      {form.processAddress}
+      {form.trustee}
+      {form.citizenship}            
+      {form.admissionNoteSendingOrganization}
+      {form.admissionNoteSender}
+      {form.sendersEmail}
+      {form.sendersPhonenumber}
+      {form.hazardAssessment}
+      {form.diaariNumber}
+      {form.datePrescribedForPsychiatricAssessment}
+      {form.nativeLanguage}
+      {form.desiredLanguageOfBusiness}
+      {form.municipalityOfResidence}
+      {form.prosecuted}
+      {form.deadlineForProsecution}
+      {form.preTrialPoliceDepartment}
+      {form.emailFromTheDirectorOfInvestigation}
+      {form.phonenumberFromTheDirectorOfInvestigation}
+      {form.addressFromTheDirectorOfInvestigation}
+      {form.crime}
+      {form.crimes}
+      {form.assistantsEmail}
+      {form.assistantsPhonenumber}
+      {form.assistantsAddress}
+      {form.legalGuardianEmail}
+      {form.legalGuardianPhonenumber}
+      {form.legalGuardianAddress}
+      {form.legalGuardianInstitute}
+      {form.appealedDecision}
     </div>
 }
 
