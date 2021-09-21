3a27293ba3a208acf4e7617627ae58eb663cc27b
uradora <meri.rajamaki@gmail.com>
2021-09-21 07:03:22 +0300

    lint virheet korjattu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,55 +6,55 @@ import THLForm from './components/THLForm'
 
 const App = () => {
 
-  const isTHL = process.env.ENV_THL
+    const isTHL = process.env.ENV_THL
 
     if (isTHL) {
 
-      const [forms, setForms] = useState([])
+        const [forms, setForms] = useState([])
 
-      useEffect(() => {
+        useEffect(() => {
         addmissionService.getAll().then(response => {
-          setForms(response.data)
+            setForms(response.data)
+          })
         })
-      })
 
-      return (
-        <div>
-          <h2>Lomakkeet:</h2>
-            {forms.map(form =>
-              <THLForm key={form.id}
-                form={form}
-              />
-            )}
-        </div>
-      )
+        return (
+            <div>
+                <h2>Lomakkeet:</h2>
+                {forms.map(form =>
+                    <THLForm key={form.id}
+                        form={form}
+                    />
+                )}
+            </div>
+        )
 
     } else {
-      
-      const [addmissions, setAddmissions] = useState([])
+
+        const [addmissions, setAddmissions] = useState([])
 
 
-      useEffect(() => {
-        addmissionService
-          .getAll().then(response => {
-            setAddmissions(response.data)
-          })
+        useEffect(() => {
+            addmissionService
+                .getAll().then(response => {
+                    setAddmissions(response.data)
+                })
         }, [])
 
-      const addmissionForm = () => (
+        const addmissionForm = () => (
 
-        <AddmissionForm createAddmission={addNewAddmission} />
-      )
+            <AddmissionForm createAddmission={addNewAddmission} />
+        )
 
 
-      const addNewAddmission = async (addmissionObject) => {
+        const addNewAddmission = async (addmissionObject) => {
 
-        addmissionService
-          .create(addmissionObject)
-          .then(response => {
-            setAddmissions(addmissions.concat(response.data))
-            console.log('App filen addnewaddmission funktiossa')
-          })
+            addmissionService
+                .create(addmissionObject)
+                .then(response => {
+                    setAddmissions(addmissions.concat(response.data))
+                    console.log('App filen addnewaddmission funktiossa')
+                })
 
         /*try {
         const newAddmission = await addmissionService.create(addmissionObject)
@@ -64,16 +64,16 @@ const App = () => {
         catch (exception){
             console.log('Virhe')
         }*/
-    }
+      }
 
-    return (
-      <div>
-        <h2>Lisää henkilö:</h2>
-          {addmissionForm()}
-        <p></p>
-      </div>
-    )
-  }
+      return (
+          <div>
+              <h2>Lisää henkilö:</h2>
+              {addmissionForm()}
+              <p></p>
+          </div>
+      )
+    }
 
 }
 
