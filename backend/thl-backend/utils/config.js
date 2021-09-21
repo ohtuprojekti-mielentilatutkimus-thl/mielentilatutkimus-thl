4a81f735c0faf56786704e08a1a2736c45ab745d
Jussi M <jussmaki@cs.helsinki.fi>
2021-09-21 14:53:35 +0300

    Remove thl-backend folder

--- backend/thl-backend/utils/config.js
+++
@@ -1,9 +0,0 @@
-require ('dotenv').config()
-
-let env = process.env.NODE_ENV
-
-let MONGODB_URI = env === 'prod' ? 'mongodb://mielentila_db:27017/mielentila' : 'mongodb://localhost:27017/mielentila'
-
-module.exports = {
-    MONGODB_URI
-}
