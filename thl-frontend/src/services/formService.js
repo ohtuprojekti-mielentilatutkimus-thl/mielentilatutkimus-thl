537455848ebea2fca022fd6c512ad8bef4ddf6ee
uradora <meri.rajamaki@gmail.com>
2021-09-24 06:06:03 +0300

    poistettu commit-sotkua

--- thl-frontend/src/services/formService.js
+++ thl-frontend/src/services/formService.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios'
-const baseUrl = '/admission_form'
-
-
-const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
-}
-
-export default { getAll }
-=======
 import axios from 'axios'
 const baseUrl = '/admissions'
 
@@ -20,4 +8,3 @@ const getAll = () => {
 }
 
 export default { getAll }
->>>>>>> a3292d60859567013bdafa1de2b71cd12dceb4a9
