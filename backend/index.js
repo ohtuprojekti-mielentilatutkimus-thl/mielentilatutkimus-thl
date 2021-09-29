cd2163babf03299edeb874c44ab2434493000c98
ArtKoski <arcez@hotmail.fi>
2021-09-29 16:27:34 +0300

    add: mailDev

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 const app = require('./app')
 
-
+const logger = require('./utils/logger')
 const http = require('http')
 const server = http.createServer(app)
+
+const MailDev = require('maildev')
+
+const maildev = new MailDev()
+
+maildev.listen()
+
+// Handle new emails as they come in
+maildev.on('new', (email) => {
+    logger.info(`Received new email with subject: ${email.subject}`)
+})
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
