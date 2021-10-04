ece61e24a64621f905d13db00ceb96a9c76dc5a3
uradora <meri.rajamaki@gmail.com>
2021-10-04 23:40:44 +0300

    thl form styles, will edit later

--- thl-frontend/src/components/AdmissionForm.js
+++ thl-frontend/src/components/AdmissionForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import formService from '../services/formService'
+import { Paper, Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
 const FormState = (form) => {
 
@@ -23,14 +25,13 @@ const FormState = (form) => {
 
     return (
         <div>
-
             <form onSubmit={changeFormState}>
                 <div className='states'>
                     <label>
                         <input type='radio' value='Hyväksytty/Tarkastettu'
                             checked={selectedOption === 'Hyväksytty/Tarkastettu'}
                             onChange={handleOptionChange} />
-          Hyväksytty/Tarkastettu
+                                Hyväksytty/Tarkastettu
                     </label>
                 </div>
                 <div className='states'>
@@ -88,162 +89,202 @@ const AdmissionForm = ({ form }) => {
         setFormState(event.target.value)
     }*/
 
+    const useStyles = makeStyles({
+        form: {
+            display: 'center',
+            background: 'white',
+            padding: '10px',
+            borderWidth: '1px',
+            width: '50%',
+            height: '50%',
+            align: 'center',
+            justifyContent: 'center'
+        },
+        text: {
+            fontSize: '18px',
+            justifyContent: 'center',
+            align: 'center'
+        }
+    })
 
+    const classes = useStyles()
 
     if (showInfo) {
         return (
-            <div>
-                <h1>Lomake: {form.id}</h1>
-                <div>
-                  Lomakkeen tila:
-                    {form.formState}
-                </div>
-                <FormState form={form}/>
-                <h2>Yleiset tutkittavan henkilön tiedot:</h2>
-                <br />
-                <div>
-                    Etunimet:
-                    {form.name}
-                </div>
-                <div>
-                    Sukunimi:
-                    {form.lastname}
-                </div>
-                <div>
-                    Henkilötunnus:
-                    {form.identificationNumber}
-                </div>
-                <div>
-                    Kotiosoite:
-                    {form.address}
-                </div>
-                <div>
-                    Sijainti:
-                    {form.location}
-                </div>
-                <div>
-                    Prosessiosoite:
-                    {form.processAddress}
-                </div>
-                <div>
-                    Edunvalvoja:
-                    {form.trustee}
-                </div>
-                <div>
-                    Kansalaisuus:
-                    {form.citizenship}
-                </div>
-                <div>
-                    Tutkimuspyynnön lähettävä taho:
-                    {form.admissionNoteSendingOrganization}
-                </div>
-                <div>
-                    Tutkimuspyynnön lähettävä henkilö:
-                    {form.admissionNoteSender}
-                </div>
-                <div>
-                    Tutkimuspyynnön lähettäjän sähköposti:
-                    {form.sendersEmail}
-                </div>
-                <div>
-                    Tutkimuspyynnön lähettäjän puhelinnumero:
-                    {form.sendersPhonenumber}
-                </div>
-                <br />
-                <h2>Mielentilatutkimuslomake:</h2>
-                <br />
-                <div>
-                    Halutaanko lisäksi vaarallisuusarvio:
-                    {form.hazardAssessment}
-                </div>
-                <div>
-                    Diaarinumero:
-                    {form.diaariNumber}
-                </div>
-                <div>
-                    Päivämäärä, jolla oikeus on määrännyt tutkittavan mielentilatutkimukseen:
-                    {form.datePrescribedForPsychiatricAssessment}
-                </div>
-                <div>
-                    Tutkittavan äidinkieli:
-                    {form.nativeLanguage}
-                </div>
-                <div>
-                    Tutkittavan toivoma asiointikieli:
-                    {form.desiredLanguageOfBusiness}
-                </div>
-                <div>
-                    Tutkittavan kotikunta:
-                    {form.municipalityOfResidence}
-                </div>
-                <div>
-                    Onko syyte nostettu:
-                    {form.prosecuted}
-                </div>
-                <div>
-                    Jos syytettä ei ole nostettu, syytteen nostamisen määräaika:
-                    {form.deadlineForProsecution}
-                </div>
-                <div>
-                    Jos syytettä ei ole nostettu, esitutkinnan suorittava poliisilaitos:
-                    {form.preTrialPoliceDepartment}
-                </div>
-                <div>
-                    Jos syytettä ei ole nostettu, tutkinnan johtajan sähköposti:
-                    {form.emailFromTheDirectorOfInvestigation}
-                </div>
-                <div>
-                    Jos syytettä ei ole nostettu, tutkinnan johtajan puhelinnumero:
-                    {form.phonenumberFromTheDirectorOfInvestigation}
-                </div>
-                <div>
-                    Jos syytettä ei ole nostettu, tutkinnan johtajan osoite:
-                    {form.addressFromTheDirectorOfInvestigation}
-                </div>
-                <div>
-                    Mielentilatutkimuksen määräämiseen johtanut vakavin teko (päätös tai välituomio):
-                    {form.crime}
-                </div>
-                <div>
-                    Muut kyseessä olevat teot, joista mielentilatutkimusta pyydetään:
-                    {form.crimes}
-                </div>
-                <div>
-                    Tutkittavan avustajan sähköposti:
-                    {form.assistantsEmail}
-                </div>
-                <div>
-                    Tutkittavan avustajan puhelinnumero:
-                    {form.assistantsPhonenumber}
-                </div>
-                <div>
-                    Tutkittavan avustajan osoite:
-                    {form.assistantsAddress}
-                </div>
-                <div>
-                    Alaikäisen tutkittavan huoltajan/sosiaalitoimen sähköposti:
-                    {form.legalGuardianEmail}
-                </div>
-                <div>
-                    Alaikäisen tutkittavan huoltajan/sosiaalitoimen puhelinnumero:
-                    {form.legalGuardianPhonenumber}
-                </div>
-                <div>
-                    Alaikäisen tutkittavan huoltajan/sosiaalitoimen osoite:
-                    {form.legalGuardianAddress}
-                </div>
-                <div>
-                    Alaikäisen tutkittavan huoltajan/sosiaalitoimen mahdollinen laitos:
-                    {form.legalGuardianInstitute}
-                </div>
-                <div>
-                    Mikäli lähettäjä hovioikeus/korkein oikeus, mihin päätökseen haettu muutosta:
-                    {form.appealedDecision}
-                </div>
-                <a href='#' onClick={() => handleShowLessInfo()}>
-                    Sulje lomake
-                </a>
-
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <Paper
+                    className={classes.form}
+                    variant='outlined'
+                    elevation={3}
+                    square={false}
+                    align='center'
+                    justify='center'
+                >
+                    <h1>Lomake: {form.id}</h1>
+                    <div>
+                        Lomakkeen tila:
+                        {form.formState}
+                    </div>
+                    <FormState form={form}/>
+                    <h2>Yleiset tutkittavan henkilön tiedot:</h2>
+                    <br />
+                    <Grid
+                        container rowSpacing={2}
+                        columnSpacing={{ xs: 2 }}
+                    >
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Etunimet:</div>
+                            <div className={classes.text}>{form.name}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Sukunimi:</div>
+                            <div className={classes.text}>{form.lastname}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Henkilötunnus:</div>
+                            <div className={classes.text}>{form.identificationNumber}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Kotiosoite:</div>
+                            <div className={classes.text}>{form.address}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Sijainti:</div>
+                            <div className={classes.text}>{form.location}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Prosessiosoite:</div>
+                            <div className={classes.text}>{form.processAddress}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Edunvalvoja:</div>
+                            <div className={classes.text}>{form.trustee}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Kansalaisuus:</div>
+                            <div className={classes.text}>{form.citizenship}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Tutkimuspyynnön lähettävä taho:</div>
+                            <div className={classes.text}>{form.admissionNoteSendingOrganization}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Tutkimuspyynnön lähettävä henkilö:</div>
+                            <div className={classes.text}>{form.admissionNoteSender}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Tutkimuspyynnön lähettäjän sähköposti:</div>
+                            <div className={classes.text}>{form.sendersEmail}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Tutkimuspyynnön lähettäjän puhelinnumero:</div>
+                            <div className={classes.text}>{form.sendersPhonenumber}</div>
+                        </Grid>
+                    </Grid>
+                    <br />
+                    <h2>Mielentilatutkimuslomake:</h2>
+                    <br />
+                    <Grid
+                        container rowSpacing={2}
+                        columnSpacing={{ xs: 2 }}
+                    >
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Halutaanko lisäksi vaarallisuusarvio:</div>
+                            <div className={classes.text}>{form.hazardAssessment}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Diaarinumero:</div>
+                            <div className={classes.text}>{form.diaariNumber}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Päivämäärä, jolla oikeus on määrännyt tutkittavan mielentilatutkimukseen:</div>
+                            <div className={classes.text}>{form.datePrescribedForPsychiatricAssessment}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Tutkittavan äidinkieli:</div>
+                            <div className={classes.text}>{form.nativeLanguage}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Tutkittavan toivoma asiointikieli:</div>
+                            <div className={classes.text}>{form.desiredLanguageOfBusiness}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Tutkittavan kotikunta:</div>
+                            <div className={classes.text}>{form.municipalityOfResidence}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Onko syyte nostettu:</div>
+                            <div className={classes.text}>{form.prosecuted}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Jos syytettä ei ole nostettu, syytteen nostamisen määräaika:</div>
+                            <div className={classes.text}>{form.deadlineForProsecution}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Jos syytettä ei ole nostettu, esitutkinnan suorittava poliisilaitos:</div>
+                            <div className={classes.text}>{form.preTrialPoliceDepartment}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Jos syytettä ei ole nostettu, tutkinnan johtajan sähköposti:</div>
+                            <div className={classes.text}>{form.emailFromTheDirectorOfInvestigation}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Jos syytettä ei ole nostettu, tutkinnan johtajan puhelinnumero:</div>
+                            <div className={classes.text}>{form.phonenumberFromTheDirectorOfInvestigation}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Jos syytettä ei ole nostettu, tutkinnan johtajan osoite:</div>
+                            <div className={classes.text}>{form.addressFromTheDirectorOfInvestigation}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Mielentilatutkimuksen määräämiseen johtanut vakavin teko (päätös tai välituomio):</div>
+                            <div className={classes.text}>{form.crime}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Muut kyseessä olevat teot, joista mielentilatutkimusta pyydetään:</div>
+                            <div className={classes.text}>{form.crimes}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Tutkittavan avustajan sähköposti:</div>
+                            <div className={classes.text}>{form.assistantsEmail}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Tutkittavan avustajan puhelinnumero:</div>
+                            <div className={classes.text}>{form.assistantsPhonenumber}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Tutkittavan avustajan osoite:</div>
+                            <div className={classes.text}>{form.assistantsAddress}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Alaikäisen tutkittavan huoltajan/sosiaalitoimen sähköposti:</div>
+                            <div className={classes.text}>{form.legalGuardianEmail}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Alaikäisen tutkittavan huoltajan/sosiaalitoimen puhelinnumero:</div>
+                            <div className={classes.text}>{form.legalGuardianPhonenumber}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Alaikäisen tutkittavan huoltajan/sosiaalitoimen osoite:</div>
+                            <div className={classes.text}>{form.legalGuardianAddress}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Alaikäisen tutkittavan huoltajan/sosiaalitoimen mahdollinen laitos:</div>
+                            <div className={classes.text}>{form.legalGuardianInstitute}</div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div className={classes.text}>Mikäli lähettäjä hovioikeus/korkein oikeus, mihin päätökseen haettu muutosta:</div>
+                            <div className={classes.text}>{form.appealedDecision}</div>
+                        </Grid>
+                    </Grid>
+                    <a href='#' onClick={() => handleShowLessInfo()}>
+                        Sulje lomake
+                    </a>
+                </Paper>
             </div>
         )} else {
         return (
