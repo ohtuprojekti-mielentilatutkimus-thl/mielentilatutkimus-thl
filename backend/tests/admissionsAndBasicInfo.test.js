5ac363e93c413618226b36d2b966162535dd7bda
ArtKoski <arcez@hotmail.fi>
2021-10-05 13:39:23 +0300

    add backend test for changing admission forms 'formState'

--- backend/tests/admissionsAndBasicInfo.test.js
+++ backend/tests/admissionsAndBasicInfo.test.js
@@ -25,52 +25,69 @@ describe('when db is initialized with data', () => {
         await newAdmissionForm.save()
 
     })    
+    describe('basic information..', () => { 
     
-    test('basic information is returned as json', async () => {
-        await api
-            .get(baseUrl+'/basic_information/anything')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-    })
-
-    test('basic information can be retrieved from db', async () => {
-        const basicsInDb = await helper.basicsInDb()
+        test('is returned as json', async () => {
+            await api
+                .get(baseUrl+'/basic_information/anything')
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
+        })
+        test('can be retrieved from db', async () => {
+            const basicsInDb = await helper.basicsInDb()
         
-        // +1 for id
-        const lengthOfInputFields = Object.keys(helper.basic_information_input).length + 1 
-        const lengthOfFieldsInDbItem = Object.keys(basicsInDb[0])
-        expect(lengthOfFieldsInDbItem).toHaveLength(lengthOfInputFields)
-    })
+            // +1 for id
+            const lengthOfInputFields = Object.keys(helper.basic_information_input).length + 1 
+            const lengthOfFieldsInDbItem = Object.keys(basicsInDb[0])
+            expect(lengthOfFieldsInDbItem).toHaveLength(lengthOfInputFields)
+        })
 
-    test('basic information can be retrieved with GET', async () => {
-        const basicsInDb = await helper.basicsInDb()
-        const idOfItemInDb = basicsInDb[0].id
+        test('can be retrieved with GET', async () => {
+            const basicsInDb = await helper.basicsInDb()
+            const idOfItemInDb = basicsInDb[0].id
 
-        const response = await api.get(baseUrl+'/basic_information/'+idOfItemInDb)
+            const response = await api.get(baseUrl+'/basic_information/'+idOfItemInDb)
         
-        const lengthOfInputFields = Object.keys(helper.basic_information_input).length + 1 
-        expect(Object.keys(response.body[0])).toHaveLength(lengthOfInputFields)
+            const lengthOfInputFields = Object.keys(helper.basic_information_input).length + 1 
+            expect(Object.keys(response.body[0])).toHaveLength(lengthOfInputFields)
+        })
     })
-    
-    
-    test('(singular) admission form information can be retrieved from db', async () => {
-        const admissionsInDb = await helper.admissionsInDb()
+    describe('admission form information..', () => { 
+   
+        test('can be retrieved from db', async () => {
+            const admissionsInDb = await helper.admissionsInDb()
         
-        // +1 for id
-        const lengthOfInputFields = Object.keys(helper.admission_form_input).length + 1 
-        const lengthOfFieldsInDbItem = Object.keys(admissionsInDb[0])
+            // +1 for id
+            const lengthOfInputFields = Object.keys(helper.admission_form_input).length + 1 
+            const lengthOfFieldsInDbItem = Object.keys(admissionsInDb[0])
 
-        expect(lengthOfFieldsInDbItem).toHaveLength(lengthOfInputFields)
-    })    
+            expect(lengthOfFieldsInDbItem).toHaveLength(lengthOfInputFields)
+        })    
 
-    test('admission form information can be retrieved with GET', async () => {
-        const admissionsInDb = await helper.admissionsInDb()
-        const response = await api.get(baseUrl+'/')
+        test('can be retrieved with GET', async () => {
+            const admissionsInDb = await helper.admissionsInDb()
+            const response = await api.get(baseUrl+'/')
         
-        const lengthOfItems = admissionsInDb.length 
-        expect(response.body).toHaveLength(lengthOfItems)
+            const lengthOfItems = admissionsInDb.length 
+            expect(response.body).toHaveLength(lengthOfItems)
+        })
+
+        test('field ´formState´ can be changed with PUT', async () => {
+            let admissionsInDb = await helper.admissionsInDb()
+            const idOfItemInDb = admissionsInDb[0].id
+            
+            const changedAdmissionForm = { ...helper.admission_form_input, 
+                formState: 'muutettu prosessin tila'}
+
+            await api
+                .put(baseUrl+'/thl/'+idOfItemInDb)
+                .send(changedAdmissionForm)
+
+            admissionsInDb = await helper.admissionsInDb()
+            const updatedAdmissionForm = admissionsInDb.find(item => item.id===idOfItemInDb)
+            expect(updatedAdmissionForm.formState).toBe('muutettu prosessin tila')
+        })
     })
-
 })
 
 describe('when db is empty', () => {
@@ -107,6 +124,8 @@ describe('when db is empty', () => {
 
         expect(lengthOfFieldsInDbItem).toHaveLength(lengthOfInputFields)
     })
+
+
 })
 
 
