442b3dc5ecf879029457b06c0052271835f50410
johannaval <johanna.valminen@gmail.com>
2021-10-06 13:10:58 +0300

    THL-frontend cypress ypress tests works

--- thl-frontend/cypress/integration/listingAdmissions.spec.js
+++ thl-frontend/cypress/integration/listingAdmissions.spec.js
@@ -1,22 +1,45 @@
 /* eslint-disable no-undef */
 
-describe('when db is initialized with data', () => {
+describe('Admissions can be viewed', () => {
+
+    beforeEach(function() {
+
+        var senders_id = ''
+
+        cy.request('POST', 'http://localhost:3001/api/admissions/basic_information_form', {
+            admissionNoteSender: 'Sampo',
+            admissionNoteSenderOrganization: 'Organisaatio',
+            sendersEmail: 'postia@sampolle.fi',
+            sendersPhoneNumber: '060606606060'
+        }).then(response => {
+
+            localStorage.setItem('sender_id', JSON.stringify(response.body.id))
+            const sender_id = localStorage.sender_id
+            senders_id = sender_id.replace(/['"]+/g,'')
+            console.log(senders_id)
+
+            //   cy.request('POST', `http://localhost:3001/api/admissions/admission_form/${senders_id}`, {
+            cy.request('POST', 'http://localhost:3001/api/admissions/admission_form', {
+                admissionNoteSender: 'Sampo2',
+                diaariNumber: '123456789',
+            })
+        })
+    })
+
 
     it('Admission can be viewed', function () {
 
-        cy.visit('http://localhost:3002/thl-admissions')
+        cy.visit('http://localhost:3001/thl-admissions')
         cy.contains('Lomakkeet')
 
         cy.get('a').last().click()
         cy.contains('Yleiset tutkittavan henkilön tiedot')
-
-        cy.contains('Outi')
-        cy.contains('666')
+        cy.contains('123456789')
 
         cy.get('#handleShowLessInfo').click()
         cy.contains('Lomakkeet')
-
-    })
+    }
+    )
 }
 )
 
