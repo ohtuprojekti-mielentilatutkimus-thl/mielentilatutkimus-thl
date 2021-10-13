bd6d5348cb3b0e2d9b8867f280f6903d26e7bdfa
Jussi M <jussmaki@cs.helsinki.fi>
2021-10-13 18:54:51 +0300

    End-to-end test for sending attachments

---
+++ frontend/cypress/integration/sendAttachment.spec.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-undef */
+
+const helper = require('./test_helper')
+import 'cypress-file-upload'
+
+describe('Attach files in  Admission Form', function() {
+
+    it('Post basic information form to get the id', function(){
+        cy.request('POST', 'http://localhost:3000/api/admissions/basic_information_form', {
+            admissionNoteSender: helper.basic_information_input.admissionNoteSender,
+            admissionNoteSenderOrganization: helper.basic_information_input.admissionNoteSenderOrganization,
+            sendersEmail: helper.basic_information_input.sendersEmail,
+            sendersPhoneNumber: helper.basic_information_input.sendersPhoneNumber
+        }).then(response => {
+            localStorage.setItem('sender_id', JSON.stringify(response.body.id))
+            const sender_id = localStorage.sender_id
+            const senders_id = sender_id.replace(/['"]+/g,'')
+
+            cy.visit(`http://localhost:3000/admission_form/${senders_id}`)
+            cy.contains('Tutkimuspyynnön lähettäjän tiedot')
+
+            cy.intercept({
+                method: 'POST',
+                url: `/api/admissions/admission_form_attachment/${senders_id}`,
+            }).as('uploadApi')
+
+            cy.get('#name').type(helper.admission_form_input.name)
+            cy.get('#diaariNumber').type(helper.admission_form_input.diaariNumber)
+
+
+            const testFile = 'testfile.pdf'
+            const filePath = 'testfiles/' + testFile
+
+            /*cy.contains('Valitse tiedosto').click()
+            cy.get('input[type="file"]').attachFile(filepath)
+            cy.contains('Lataa valittu tiedosto').click()
+            cy.wait('@uploadApi').then((interception) => {
+                assert.equal(testfile, interception.response.body.originalname)
+                assert.equal('valituomio', interception.response.body.whichFile)
+                assert.equal('ok', interception.response.body.message)
+            })*/
+
+            const attachments = ['valituomio', 'poytakirja', 'haastehakemus', 'rikosrekisteriote', 'esitutkintapoytakirja', 'vangitsemispaatos']
+
+            for (const i in attachments) {
+                cy.get(`input[name="${attachments[i]}"]`).attachFile(filePath)
+                cy.contains('Lataa valittu tiedosto').click()
+                cy.wait('@uploadApi').then((interception) => {
+                    assert.equal(testFile, interception.response.body.originalname)
+                    assert.equal(attachments[i], interception.response.body.whichFile)
+                    assert.equal('ok', interception.response.body.message)
+                })
+            }
+
+
+            cy.get('#createPersonButton').click()
+            cy.contains('Pyyntö lähetettiin onnistuneesti')
+
+        })
+
+    })
+
+})
