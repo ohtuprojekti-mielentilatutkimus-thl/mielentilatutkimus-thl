77ed27bed786a3ee87fddbe9bf737469f77bdd1f
ktatu <kaikkonentatu@gmail.com>
2021-10-19 20:12:47 +0300

    mongoose connection closed after every test suite is ran

--- backend/tests/admissionsAndBasicInfo.test.js
+++ backend/tests/admissionsAndBasicInfo.test.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
-const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
@@ -148,10 +147,4 @@ describe('when db is empty', () => {
     })
 
 
-})
-
-
-
-afterAll(() => {
-    mongoose.connection.close()
 })
