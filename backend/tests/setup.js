a0f9cff6669ce2625ad2874288472692842ec4bd
ktatu <kaikkonentatu@gmail.com>
2021-10-20 14:54:17 +0300

    Added comment to describe setup.js's function in testing

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+// this file's code is ran in every test suite (=test file in our case) after finishing
 const mongoose = require('mongoose')
 
 afterAll(async () => {
