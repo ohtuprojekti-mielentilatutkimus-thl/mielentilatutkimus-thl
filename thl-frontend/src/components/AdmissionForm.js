1d9675e9ec376966de3dbbc055c8e1085a314bf7
uradora <meri.rajamaki@gmail.com>
2021-10-24 14:50:46 +0300

    lomake-id:n etuliite

--- thl-frontend/src/components/AdmissionForm.js
+++ thl-frontend/src/components/AdmissionForm.js
@@ -114,7 +114,7 @@ const AdmissionForm = ({ form, updateForms } ) => {
                             align='left'
                             justify='left'
                         >
-                            <h1>Lomake: {form.createdAt.substring(0,10)}</h1>
+                            <h1>THL_OIKPSYK_{form.createdAt.substring(0,10)}</h1>
                             <FormState form={form} formState={form.formState} updateForms={updateForms} />
                             <h2>Yleiset tutkittavan henkilön tiedot:</h2>
                             <br />
