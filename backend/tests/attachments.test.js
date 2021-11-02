508818810619c2ac4813f5ffa2d6b43fa611ad99
ktatu <kaikkonentatu@gmail.com>
2021-11-02 14:55:53 +0200

    Refactored basicInfo stuff out of attachment tests

--- backend/tests/attachments.test.js
+++ backend/tests/attachments.test.js
@@ -2,62 +2,98 @@
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
-
 const baseUrl = '/api/admissions'
 
 const api = supertest(app)
 
-const BasicInformationForm = require('../models/basicInformationForm.model.js')
+const AdmissionForm = require('../models/admissionForm.model.js')
 const AttachmentForm = require('../models/attachmentForm.model.js')
 
 const fs = require('fs')
 const path = require('path')
 
+let testAdmissionId
+
+beforeAll(async () => {
+    await AdmissionForm.deleteMany({})
+
+    const newAdmissionForm = new AdmissionForm(helper.admissionFormTestData)
+    await newAdmissionForm.save()
+
+    const admissionsInDb = await helper.admissionsInDb()
+    testAdmissionId = admissionsInDb[0].id
+})
+
 beforeEach(async () => {
     await AttachmentForm.deleteMany({})
-    await BasicInformationForm.deleteMany({})
 })    
 
-test('attachment can be sent by a valid user', async () => {
-    const newBasicsForm = new BasicInformationForm(helper.basicInfoFormTestData)
-    await newBasicsForm.save()
-    const basicsInDb = await helper.basicsInDb()
-    const basics = basicsInDb[0]
+describe('when sending an attachment with post-request', () => {
 
-    await api
-        .post(baseUrl+`/admission_form_attachment/${basics.id}`)
-        .attach('file', fs.createReadStream(path.join(__dirname, '../attachments/selenium-screenshot-62.png')))
-        .expect(200)
-      
-    const attachmentsInDb = await helper.attachmentsInDb()
-
-    expect(attachmentsInDb).toHaveLength(1)
-})
-
-test('attachment can\'t be sent without a user', async () => {
-    await api
-        .post(baseUrl+'/admission_form_attachment')
-        .attach('file', fs.createReadStream(path.join(__dirname, '../attachments/selenium-screenshot-62.png')))
-
-    const attachmentsInDb = await helper.attachmentsInDb()
+    test('attachment can be sent by a valid user', async () => {
+        await api
+            .post(baseUrl+`/admission_form_attachment/${testAdmissionId}`)
+            .attach('file', fs.createReadStream(path.join(__dirname, '../attachments/selenium-screenshot-62.png')))
+            .expect(200)
+          
+        const attachmentsInDb = await helper.attachmentsInDb()
     
-    expect(attachmentsInDb).toHaveLength(0)
-
+        expect(attachmentsInDb).toHaveLength(1)
+    })
+    
+    test('attachment can\'t be sent without a user', async () => {
+        await api
+            .post(baseUrl+'/admission_form_attachment')
+            .attach('file', fs.createReadStream(path.join(__dirname, '../attachments/selenium-screenshot-62.png')))
+    
+        const attachmentsInDb = await helper.attachmentsInDb()
+        
+        expect(attachmentsInDb).toHaveLength(0)
+    
+    })
+    
+    test('attachments have a field describing which file it is', async () => {
+        await api
+            .post(baseUrl+`/admission_form_attachment/${testAdmissionId}`)
+            .attach('file', fs.createReadStream(path.join(__dirname, '../attachments/selenium-screenshot-62.png')))
+            .expect(200)
+          
+        const attachmentsInDb = await helper.attachmentsInDb()
+    
+        expect(attachmentsInDb[0].whichFile).not.toBeNull()
+    })
 })
 
-test('attachments have a field describing which file it is', async () => {
-    const newBasicsForm = new BasicInformationForm(helper.basicInfoFormTestData)
-    await newBasicsForm.save()
-    const basicsInDb = await helper.basicsInDb()
-    const basics = basicsInDb[0]
+describe('on requesting an attachment with get-request,', () => {
 
+    test('pdf file can be sent to requesting client', async () => {
+        await postTestPdf()
+        const testPdf = await AttachmentForm.findOne({ fileName: 'test_pdf.pdf' })
+    
+        await api
+            .get(baseUrl+`/admission_form_attachment/${testPdf.id}`)
+            .expect(200)
+            .expect('Content-Type', /application\/pdf/)
+    })
+    
+    test('tmp folder exists after a request', () => {
+        fs.mkdir(path.join(__dirname, '../tmp'), (err) => {
+            expect(err.code).toBe('EEXIST')
+        })
+    })
+    
+    test('tmp folder is empty after a request', () => {
+        fs.readdir(path.join(__dirname, '../tmp'), (err, files) => {
+            expect(files).toHaveLength(0)
+        })
+    })  
+})
+
+const postTestPdf = async () => {
     await api
-        .post(baseUrl+`/admission_form_attachment/${basics.id}`)
-        .attach('file', fs.createReadStream(path.join(__dirname, '../attachments/selenium-screenshot-62.png')))
+        .post(baseUrl+`/admission_form_attachment/${testAdmissionId}`)
+        .attach('file', fs.createReadStream(path.join(__dirname, '../attachments/test_pdf.pdf')))
+        .field('whichFile', 'valituomio')
+        .field('originalname', 'test_pdf.pdf')
         .expect(200)
-      
-    const attachmentsInDb = await helper.attachmentsInDb()
-
-    expect(attachmentsInDb[0].whichFile).not.toBeNull()
-
-})
+}
