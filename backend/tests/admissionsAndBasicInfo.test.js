ff484569ff7908abd8a87caa4817c32f47b4d0b7
ArtKoski <arcez@hotmail.fi>
2021-11-03 12:27:20 +0200

    add tests for empty input in basicInfoForm

--- backend/tests/admissionsAndBasicInfo.test.js
+++ backend/tests/admissionsAndBasicInfo.test.js
@@ -114,55 +114,90 @@ describe('when db is initialized with data', () => {
     })
 })
 
+
 describe('when db is empty', () => {
 
     beforeEach(async () => {
         await AdmissionForm.deleteMany({})
         await BasicInformationForm.deleteMany({})
     })
-    test('basic information can be saved to database with POST', async () => {
+    describe('basic information', () => {
         const basicInfo = helper.basicInfoFormTestData
 
-        await api
-            .post(baseUrl+'/basic_information_form')
-            .send(basicInfo)        
+        test('can be saved to database with POST', async () => {
+
+            await api
+                .post(baseUrl+'/basic_information_form')
+                .send(basicInfo)        
       
-        const basicsInDb = await helper.basicsInDb()
+            const basicsInDb = await helper.basicsInDb()
 
-        expect(basicsInDb).toHaveLength(1)
-        for (k in basicInfo) {
-            expect(basicInfo[k]).not.toBeNull()
-            expect(basicInfo[k]).not.toBeUndefined()
-            expect(basicInfo[k]).toEqual(basicsInDb[0][k])
-        }
+            expect(basicsInDb).toHaveLength(1)
+            for (k in basicInfo) {
+                expect(basicInfo[k]).not.toBeNull()
+                expect(basicInfo[k]).not.toBeUndefined()
+                expect(basicInfo[k]).toEqual(basicsInDb[0][k])
+            }
 
-        expect(Object.keys(basicInfo).length).toBe(Object.keys(basicsInDb[0]).length-1)//-id field
+            expect(Object.keys(basicInfo).length).toBe(Object.keys(basicsInDb[0]).length-1)//-id and attachments field
 
+        })
+        describe('cant be saved to database when', () => {
+            
+            const basicInfoEmpty = {
+                'admissionNoteSenderOrganization': '',
+                'admissionNoteSender': '',
+                'sendersEmail': '',
+                'sendersPhoneNumber': ''
+            }
+
+            test('all input fields are empty', async () => {
+
+                await api
+                    .post(baseUrl+'/basic_information_form')
+                    .send(basicInfoEmpty)
+                    .expect(400)
+            })
+
+            describe('fields are valid except..', () => {
+                
+                for (var key of Object.keys(basicInfo)) {
+                    test(`input field ${key} is empty`, async () => {
+                        await api
+                            .post(baseUrl+'/basic_information_form')
+                            .send({...basicInfo, [key]: ''})
+                            .expect(400)
+                    })
+                }
+            })
+        })
+    })
+    describe('admission form', () => {
+    
+        test('can be saved to database with POST', async () => {
+            const admission_form = helper.admissionFormTestData
+
+            await api
+                .post(baseUrl+'/admission_form')
+                .send(admission_form)        
+  
+            const admissionsInDb = await helper.admissionsInDb()
+
+            expect(admissionsInDb).toHaveLength(1)
+            // + 1 for id, + 1 because formState has a default value
+            // +2 for createdAt and updatedAt
+            //+1 for attachments
+            const lengthOfInputFields = Object.keys(helper.admissionFormTestData).length + 5
+            const lengthOfFieldsInDbItem = Object.keys(admissionsInDb[0])
+
+            expect(lengthOfFieldsInDbItem).toHaveLength(lengthOfInputFields)
+
+            for (k in admission_form) {
+                expect(admission_form[k]).not.toBeNull()
+                expect(admission_form[k]).not.toBeUndefined()
+                expect(admission_form[k]).toEqual(admissionsInDb[0][k])
+            }
+        })
     })
     
-    test('admission form can be saved to database with POST', async () => {
-        const admission_form = helper.admissionFormTestData
-
-        await api
-            .post(baseUrl+'/admission_form')
-            .send(admission_form)        
-  
-        const admissionsInDb = await helper.admissionsInDb()
-
-        expect(admissionsInDb).toHaveLength(1)
-        // + 1 for id, + 1 because formState has a default value
-        // +2 for createdAt and updatedAt
-        //+1 for attachments
-        const lengthOfInputFields = Object.keys(helper.admissionFormTestData).length + 5
-        const lengthOfFieldsInDbItem = Object.keys(admissionsInDb[0])
-
-        expect(lengthOfFieldsInDbItem).toHaveLength(lengthOfInputFields)
-
-        for (k in admission_form) {
-            expect(admission_form[k]).not.toBeNull()
-            expect(admission_form[k]).not.toBeUndefined()
-            expect(admission_form[k]).toEqual(admissionsInDb[0][k])
-        }
-    })
-
 })
