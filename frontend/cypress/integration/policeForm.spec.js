eccd45ca93d90150080d0e4f2c3db52b9525db46
ktatu <kaikkonentatu@gmail.com>
2021-11-21 17:28:44 +0200

    Cypress test fixes

--- frontend/cypress/integration/policeForm.spec.js
+++ frontend/cypress/integration/policeForm.spec.js
@@ -1,18 +1,164 @@
 /* eslint-disable no-undef */
+const helper = require('./test_helper')
+import 'cypress-file-upload'
 
-describe('Police View', function() {
-    beforeEach(function() {
-        cy.visit('http://localhost:3000/mielentilatutkimus/upload_form')
+
+describe('From posting basic informations to police adding attachments', function() {
+
+    var admissionId = ''
+
+    it('adding admission', function(){
+
+        cy.request('DELETE', 'http://127.0.0.1:1080/email/all').then((res) => {
+            expect(res.status).equal(200)
+
+            cy.request('POST', 'http://localhost:3000/api/admissions/basic_information_form', {
+                admissionNoteSender: helper.basic_information_input.admissionNoteSender,
+                admissionNoteSenderOrganization: helper.basic_information_input.admissionNoteSenderOrganization,
+                sendersEmail: helper.basic_information_input.sendersEmail,
+                sendersPhoneNumber: helper.basic_information_input.sendersPhoneNumber
+
+            }).then(() => {
+                cy.wait(1000)
+                cy.request('GET', 'http://127.0.0.1:1080/email').then((emails) => {
+                    const parts = emails.body[0].text.split('/')
+                    const id_from_email = parts[parts.length-1].replace('\n','')
+                    localStorage.setItem('sender_id', JSON.stringify(id_from_email))
+                    const sender_id = localStorage.sender_id
+                    const senders_id = sender_id.replace(/['"]+/g,'')
+
+                    cy.visit(`http://localhost:3000/admission_form/${senders_id}`)
+
+                    cy.get('#date-picker')
+                        .clear()
+                        .type('\'22/02/2022\'')
+
+                    cy.get('#deadlineDate')
+                        .clear()
+                        .type('\'11/11/2021\'')
+
+                    var whichboolean = 0
+                    for (const i in helper.admission_form_input) {
+                        if (['basicInformationId', 'formSender', 'datePrescribedForPsychiatricAssesment', 'deadlineForProsecution', 'researchUnit', 'researchUnitInformation'].includes(i) ||
+                        i.includes('Ready')) {
+                            continue
+                        }
+                        if (typeof(helper.admission_form_input[i]) === 'boolean') {
+                            if (whichboolean === 0){
+                                cy.get('#selectHazardAssesment').click()
+                                cy.get('#0')
+                                    .contains('Kyllä')
+                                    .click()
+                                whichboolean= +1
+                                continue
+                            } else {
+                                cy.get('#selectIfProsecuted').click()
+                                cy.get('#1')
+                                    .contains('Ei')
+                                    .click()
+                                continue
+                            }
+                        }
+                        cy.get('#' + i).type(helper.admission_form_input[i])
+                    }
+                    cy.get('#createPersonButton').click()
+                })
+            })
+        })
     })
 
-    it('Police view page can be opened', function() {
-        cy.contains('Liitteiden lisääminen:')
+    it('police getting email for adding attachments', function(){
+
+        cy.request('DELETE', 'http://127.0.0.1:1080/email/all').then((res) => {
+            expect(res.status).equal(200)
+
+            cy.request('POST', 'http://localhost:3000/api/admissions/upload_form', {
+                email: 'pasi.polliisi@poliisi.fi',
+                value: helper.admission_form_input.diaariNumber
+            }).then(() => {
+                cy.wait(1000)
+
+                cy.request('GET', 'http://127.0.0.1:1080/email').then((emails) => {
+
+                    expect(emails.status).equal(200)
+
+                    const parts = emails.body[0].text.split('/')
+                    const id_from_email = parts[parts.length-1].replace('\n','')
+                    localStorage.setItem('admission_id', JSON.stringify(id_from_email))
+                    const admission_id = localStorage.admission_id
+                    admissionId = admission_id.replace(/['"]+/g,'')
+
+                    assert.equal(emails.body[0].headers.to, 'pasi.polliisi@poliisi.fi')
+                    assert.equal(emails.body[0].subject, 'Liitteiden lisäämisen linkki')
+
+                    expect(emails.body[0].text.includes('Tutkimuspyyntö vastaanotettu'))
+                    expect(emails.body[0].text.includes(helper.admission_form_input.diaariNumber))
+                    expect(emails.body[0].text.includes('voit lähettää liitteitä koskien tapausta (', helper.admission_form_input.diaariNumber,')'))
+                    const email_id = emails.body[0].id
+
+                    cy.request('DELETE', `http://127.0.0.1:1080/email/${email_id}`).then((res) => {
+                        expect(res.status).equal(200)
+                    })
+                })
+            })
+        })
     })
 
-    it('Police view page has fillable forms', function() {
-        cy.get('#email').type('pasi.polliisi@poliisi.fi')
-        cy.get('#value').type('R 20 / 123')
+    it('police adding attachments', function(){
 
-        cy.get('#sendButton').click()
+        cy.visit(`http://localhost:3000/upload_form/${admissionId}`)
+        cy.contains('Lataa liitteitä')
+
+        const attachments = ['valituomio', 'poytakirja', 'haastehakemus', 'rikosrekisteriote', 'esitutkintapoytakirja', 'vangitsemispaatos']
+
+        for (const i in attachments) {
+
+            var testFile = `testfile${i}.pdf`
+            var filePath = 'testfiles/' + testFile
+
+            cy.get(`input[name="${attachments[i]}"]`).attachFile(filePath)
+        }
+        cy.contains('Lähetä').click()
+        cy.wait(1000)
+        cy.contains('Liitteiden lähetys onnistui')
+    })
+
+    it('police will not receive email if the diaari number does not exist', function(){
+
+        cy.request('DELETE', 'http://127.0.0.1:1080/email/all').then((res) => {
+            expect(res.status).equal(200)
+
+            cy.request('POST', 'http://localhost:3000/api/admissions/upload_form', {
+                email: 'pasi.polliisi@poliisi.fi',
+                value: 'ABCDE 123'
+            }).then(() => {
+                cy.wait(1000)
+
+                cy.request('GET', 'http://127.0.0.1:1080/email').then((emails) => {
+                    expect(emails.body[0]).to.be.undefined
+                })
+            })
+        })
+    })
+
+    it('if the senders domain is not @poliisi.fi, email is not received even though diaari number exists', function(){
+
+        cy.request('DELETE', 'http://127.0.0.1:1080/email/all').then((res) => {
+            expect(res.status).equal(200)
+
+            cy.visit('http://localhost:3000/mielentilatutkimus/upload_form')
+
+            cy.get('#email').type('pasi.feikkipolliisi@feikkipoliisi.fi')
+            cy.get('#value').type(helper.admission_form_input.diaariNumber)
+            cy.get('#sendButton').click()
+
+            cy.wait(1000)
+
+            cy.request('GET', 'http://127.0.0.1:1080/email').then((emails) => {
+                expect(emails.body[0]).to.be.undefined
+            })
+        })
     })
 })
+
+
--- frontend/cypress/integration/sendAttachment.spec.js
+++ frontend/cypress/integration/sendAttachment.spec.js
@@ -31,33 +31,46 @@ describe('Attach files in  Admission Form', function() {
 
                     cy.visit(`http://localhost:3000/admission_form/${senders_id}`)
                     cy.contains('Tutkimuspyynnön lähettäjän tiedot')
+
                     /*
                     cy.intercept({
                         method: 'POST',
                         url: `/api/admissions/admission_form_attachment/${senders_id}`,
                     }).as('uploadApi')
-
+*/
                     cy.get('#name').type(helper.admission_form_input.name)
                     cy.get('#diaariNumber').type(helper.admission_form_input.diaariNumber)
+                    cy.get('#assistantsEmail').type(helper.admission_form_input.assistantsEmail)
+                    cy.get('#legalGuardianEmail').type(helper.admission_form_input.legalGuardianEmail)
 
+                    cy.get('#createPersonButton').click()
+                    cy.wait(1000)
 
-                    const testFile = 'testfile.pdf'
-                    const filePath = 'testfiles/' + testFile
+                    cy.contains('Pyyntö lähetettiin onnistuneesti')
+                    cy.get('#addAttachments').click()
+                    cy.wait(1000)
+
+                    cy.contains('Lataa liitteitä')
+
 
                     const attachments = ['valituomio', 'poytakirja', 'haastehakemus', 'rikosrekisteriote', 'esitutkintapoytakirja', 'vangitsemispaatos']
 
-                    for (const i in attachments) {
-                        cy.get(`input[name="${attachments[i]}"]`).attachFile(filePath)
-                        cy.contains('Lataa valittu tiedosto').click()
-                        cy.wait('@uploadApi').then((interception) => {
+                    /*  cy.wait('@uploadApi').then((interception) => {
                             assert.equal(testFile, interception.response.body.originalname)
                             assert.equal(attachments[i], interception.response.body.whichFile)
                             assert.equal('ok', interception.response.body.message)
-                        })
+                        }) */
+
+                    for (const i in attachments) {
+
+                        var testFile = `testfile${i}.pdf`
+                        var filePath = 'testfiles/' + testFile
+
+                        cy.get(`input[name="${attachments[i]}"]`).attachFile(filePath)
                     }
-                    */
-                    cy.get('#createPersonButton').click()
-                    cy.contains('Pyyntö lähetettiin onnistuneesti')
+                    cy.contains('Lähetä').click()
+                    cy.wait(1000)
+                    cy.contains('Liitteiden lähetys onnistui')
                 })
             })
         })
