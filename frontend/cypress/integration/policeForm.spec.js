dd795723fa2e629c013dcae34043fc3d130a3f00
johannaval <johanna.valminen@gmail.com>
2021-12-01 14:59:54 +0200

    tried to fix cypress test

--- frontend/cypress/integration/policeForm.spec.js
+++ frontend/cypress/integration/policeForm.spec.js
@@ -15,55 +15,24 @@ describe('From posting basic informations to police adding attachments', functio
             cy.request('POST', 'http://localhost:3000/api/admissions/basic_information_form', {
                 admissionNoteSender: helper.basic_information_input.admissionNoteSender,
                 admissionNoteSenderOrganization: helper.basic_information_input.admissionNoteSenderOrganization,
-                sendersEmail: helper.basic_information_input.sendersEmail,
+                sendersEmail: 'leo.lahettaja@helsinki.fi',
                 sendersPhoneNumber: helper.basic_information_input.sendersPhoneNumber
 
             }).then(() => {
                 cy.wait(1000)
-                cy.request('GET', 'http://127.0.0.1:1080/email').then((emails) => {
-                    const parts = emails.body[0].text.split('/')
-                    const id_from_email = parts[parts.length-1].replace('\n','')
-                    localStorage.setItem('sender_id', JSON.stringify(id_from_email))
-                    const sender_id = localStorage.sender_id
-                    const senders_id = sender_id.replace(/['"]+/g,'')
-
-                    cy.visit(`http://localhost:3000/admission_form/${senders_id}`)
-
-                    cy.get('#date-picker')
-                        .clear()
-                        .type('\'22/02/2022\'')
-
-                    cy.get('#deadlineDate')
-                        .clear()
-                        .type('\'11/11/2021\'')
-
-                    var whichboolean = 0
-                    for (const i in helper.admission_form_input) {
-                        if (['basicInformationId', 'formSender', 'datePrescribedForPsychiatricAssesment', 'deadlineForProsecution', 'researchUnit', 'researchUnitInformation'].includes(i) ||
-                        i.includes('Ready')) {
-                            continue
-                        }
-                        if (typeof(helper.admission_form_input[i]) === 'boolean') {
-                            if (whichboolean === 0){
-                                cy.get('#selectHazardAssesment').click()
-                                cy.get('#0')
-                                    .contains('Kyllä')
-                                    .click()
-                                whichboolean= +1
-                                continue
-                            } else {
-                                cy.get('#selectIfProsecuted').click()
-                                cy.get('#1')
-                                    .contains('Ei')
-                                    .click()
-                                continue
-                            }
-                        }
-                        cy.get('#' + i).type(helper.admission_form_input[i])
-                    }
-                    cy.get('#createPersonButton').click()
+                cy.request('POST', 'http://localhost:3000/api/admissions/admission_form', {
+                    name: helper.admission_form_input.name,
+                    lastname: helper.admission_form_input.lastname,
+                    diaariNumber: helper.admission_form_input.diaariNumber,
+                    formSender: 'leo.lahettaja@helsinki.fi',
                 })
             })
+        })
+        cy.request('GET', 'http://127.0.0.1:1080/email').then((emails) => {
+
+            const parts = emails.body[1].text.split('/')
+            const id_from_email = parts[parts.length-1].replace('\n','').replace('123thl_id:','').replace(/['"]+/g,'').trim()
+            admissionId = id_from_email
         })
     })
 
@@ -71,34 +40,30 @@ describe('From posting basic informations to police adding attachments', functio
 
         cy.request('DELETE', 'http://127.0.0.1:1080/email/all').then((res) => {
             expect(res.status).equal(200)
+        })
 
-            cy.request('POST', 'http://localhost:3000/api/admissions/upload_form', {
-                email: 'pasi.polliisi@poliisi.fi',
-                value: helper.admission_form_input.diaariNumber
-            }).then(() => {
-                cy.wait(1000)
+        cy.request('POST', 'http://localhost:3000/api/admissions/upload_form', {
+            email: 'pasi.polliisi@poliisi.fi',
+            value: admissionId
 
-                cy.request('GET', 'http://127.0.0.1:1080/email').then((emails) => {
+        }).then(() => {
 
-                    expect(emails.status).equal(200)
+            cy.wait(2000)
 
-                    const parts = emails.body[0].text.split('/')
-                    const id_from_email = parts[parts.length-1].replace('\n','')
-                    localStorage.setItem('admission_id', JSON.stringify(id_from_email))
-                    const admission_id = localStorage.admission_id
-                    admissionId = admission_id.replace(/['"]+/g,'')
+            cy.request('GET', 'http://127.0.0.1:1080/email').then((emails) => {
 
-                    assert.equal(emails.body[0].headers.to, 'pasi.polliisi@poliisi.fi')
-                    assert.equal(emails.body[0].subject, 'Liitteiden lisäämisen linkki')
+                expect(emails.status).equal(200)
 
-                    expect(emails.body[0].text.includes('Tutkimuspyyntö vastaanotettu'))
-                    expect(emails.body[0].text.includes(helper.admission_form_input.diaariNumber))
-                    expect(emails.body[0].text.includes('voit lähettää liitteitä koskien tapausta (', helper.admission_form_input.diaariNumber,')'))
-                    const email_id = emails.body[0].id
+                assert.equal(emails.body[0].headers.to, 'pasi.polliisi@poliisi.fi')
+                assert.equal(emails.body[0].subject, 'Liitteiden lisäämisen linkki')
 
-                    cy.request('DELETE', `http://127.0.0.1:1080/email/${email_id}`).then((res) => {
-                        expect(res.status).equal(200)
-                    })
+                expect(emails.body[0].text.includes('Tutkimuspyyntö vastaanotettu'))
+                expect(emails.body[0].text.includes(admissionId))
+                expect(emails.body[0].text.includes('voit lähettää liitteitä koskien tapausta ( THL-id: ', admissionId, ')'))
+                const email_id = emails.body[0].id
+
+                cy.request('DELETE', `http://127.0.0.1:1080/email/${email_id}`).then((res) => {
+                    expect(res.status).equal(200)
                 })
             })
         })
@@ -123,7 +88,7 @@ describe('From posting basic informations to police adding attachments', functio
         cy.contains('Liitteiden lähetys onnistui')
     })
 
-    it('police will not receive email if the diaari number does not exist', function(){
+    it('police will not receive email if the THL-id  does not exist', function(){
 
         cy.request('DELETE', 'http://127.0.0.1:1080/email/all').then((res) => {
             expect(res.status).equal(200)
@@ -141,7 +106,7 @@ describe('From posting basic informations to police adding attachments', functio
         })
     })
 
-    it('if the senders domain is not @poliisi.fi, email is not received even though diaari number exists', function(){
+    it('if the senders domain is not @poliisi.fi, email is not received even though THL-id exists', function(){
 
         cy.request('DELETE', 'http://127.0.0.1:1080/email/all').then((res) => {
             expect(res.status).equal(200)
@@ -149,7 +114,7 @@ describe('From posting basic informations to police adding attachments', functio
             cy.visit('http://localhost:3000/mielentilatutkimus/upload_form')
 
             cy.get('#email').type('pasi.feikkipolliisi@feikkipoliisi.fi')
-            cy.get('#value').type(helper.admission_form_input.diaariNumber)
+            cy.get('#value').type(admissionId)
             cy.get('#sendButton').click()
 
             cy.wait(1000)
