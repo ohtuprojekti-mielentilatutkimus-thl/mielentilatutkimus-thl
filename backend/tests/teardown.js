c8fadf66b6aefbd13d49edecb5634fb2b618200b
ktatu <kaikkonentatu@gmail.com>
2021-12-04 22:05:36 +0200

    Updated workflow to use new test script

--- backend/tests/teardown.js
+++ backend/tests/teardown.js
@@ -1,15 +1,14 @@
 // wiping test database after all tests are finished and initializing it with test data for development use
-const supertest = require('supertest')
-const mongoose = require('mongoose')
-const app = require('../app')
-const helper = require('./test_helper')
-
-const api = supertest(app)
-
-module.exports =  async () => { 
-    if (process.env.GITHUB_ACTIONS === true || process.env.NODE_ENV !== 'test') {
+module.exports =  async () => {
+    if (process.env.GITHUB_ACTIONS || process.env.NODE_ENV !== 'test') {
         return
     }
+
+    const supertest = require('supertest')
+    const mongoose = require('mongoose')
+    const app = require('../app')
+    const helper = require('./test_helper')
+    const api = supertest(app)
 
     console.log('teardown - clearing test database and initializing it with some data')
     await mongoose.connection.dropDatabase()
@@ -27,9 +26,8 @@ module.exports =  async () => {
         await api
             .post('/api/admissions/admission_form')
             .send(admis)
-    }
+    }   
+    console.log('teardown complete')
 
     await mongoose.connection.close()
-
-    console.log('teardown complete')
 }
