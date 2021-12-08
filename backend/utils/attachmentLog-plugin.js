11e1c149fd207a245b34700955f9859335a874ac
ArtKoski <arcez@hotmail.fi>
2021-12-08 17:41:56 +0200

    fix lint

--- backend/utils/attachmentLog-plugin.js
+++ backend/utils/attachmentLog-plugin.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 const LogSchema = require('../models/log.model')
-const { getDiff } = require('./logger')
 
 const plugin = function (schema) {
     
@@ -9,30 +8,10 @@ const plugin = function (schema) {
     })
     schema.pre('save', function (next) {
         next()
-        /*
-        if (this.isNew || this.collection.collectionName === 'attachmentforms') {
-            next()
-        } else {
-            
-            const originalToJson = JSON.parse(JSON.stringify(this._original))
-            originalToJson.id = originalToJson._id.toString()
-            delete originalToJson._id
-            delete originalToJson.__v
-
-            this._diff = getDiff(JSON.parse(JSON.stringify(this)), originalToJson)
-            
-            next()
-        }*/
     })
 
     schema.methods.log = function (data)  {
-        /*
-        data.diff = {
-            original: this._original,
-            changed: this._diff
-            
-        }
-        */
+        
         data.attachmentId = this._id
         data.formId = this.admissionFormId
 
