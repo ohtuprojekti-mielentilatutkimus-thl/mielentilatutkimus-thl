8c6d0d2b25c14bf324c1dc3a7e501a50083ae49a
ktatu <kaikkonentatu@gmail.com>
2022-01-01 22:20:29 +0200

    thl id serializer tests fixed

--- backend/tests/idSerializer.test.js
+++ backend/tests/idSerializer.test.js
@@ -3,23 +3,25 @@
 const idSerializer = require('../services/idSerializer')
 
 const idStart = 'THL_OIKPSYK_'
+const currYear = new Date().getFullYear().toString()
 
 describe('idSerializer returns', () => {
 
     test('the next successive id', () => {
-        let testId = idStart + '2021-9'
+        let testId = idStart + currYear + '-9'
 
-        expect(idSerializer.getNextThlRequestId(testId)).toEqual(idStart + '2021-10')
+        expect(idSerializer.getNextThlRequestId(testId)).toEqual(idStart + currYear + '-10')
     })
 
     test('1 when year has changed between admissions', () => {
-        let testId = idStart + '-2020-10'
+        let prevYear = parseInt(currYear) - 1
+        let testId = idStart + prevYear + '-10'
 
-        expect(idSerializer.getNextThlRequestId(testId)).toEqual(idStart + '2021-1')
+        expect(idSerializer.getNextThlRequestId(testId)).toEqual(idStart + currYear + '-1')
     })
 
     test('1 when database is empty', () => {
-        const expectedId = idStart + new Date().getFullYear().toString() + '-1'
+        const expectedId = idStart + currYear + '-1'
 
         expect(idSerializer.getNextThlRequestId(null)).toEqual(expectedId)
         expect(idSerializer.getNextThlRequestId(undefined)).toEqual(expectedId)
